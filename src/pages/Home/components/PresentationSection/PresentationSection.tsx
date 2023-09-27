4b2b6338673b63b93c6008f0c3e8c1db48aa3770
Victor Faria <vitilevi@hotmail.com>
2023-09-27 14:19:01 -0300

    Fix presentation container styling

--- src/pages/Home/components/PresentationSection/PresentationSection.tsx
+++ src/pages/Home/components/PresentationSection/PresentationSection.tsx
@@ -11,18 +11,22 @@ export function PresentationSection() {
           Com o Coffee Delivery você recebe seu café onde estiver a qualquer hora
         </p>
         <div className='benefits'>
-          <BenefitCard type='cart'>
-            <span>Compra simples e segura</span>
-          </BenefitCard>
-          <BenefitCard type='package'>
-            <span>Embalagem mantém o café intacto</span>
-          </BenefitCard>
-          <BenefitCard type='timer'>
-            <span>Entrega rápica e rastreada</span>
-          </BenefitCard>
-          <BenefitCard type='coffee'>
-            <span>O café chega fresquinho até você</span>
-          </BenefitCard>
+          <div className='left-container'>
+            <BenefitCard type='cart'>
+              <span>Compra simples e segura</span>
+            </BenefitCard>
+            <BenefitCard type='timer'>
+              <span>Entrega rápica e rastreada</span>
+            </BenefitCard>
+          </div>
+          <div className='right-container'>
+            <BenefitCard type='package'>
+              <span>Embalagem mantém o café intacto</span>
+            </BenefitCard>
+            <BenefitCard type='coffee'>
+              <span>O café chega fresquinho até você</span>
+            </BenefitCard>
+          </div>
         </div>
       </CoffeeDeliveryPresentation>
       <div className='image-wrapper'>
--- src/pages/Home/components/PresentationSection/styles.ts
+++ src/pages/Home/components/PresentationSection/styles.ts
@@ -49,29 +49,24 @@ export const CoffeeDeliveryPresentation = styled.div`
     line-height: 130%;
   }
 
-  .benefits-wrapper {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: 16px;
-
-    @media ${breakpoints.dt} {
-      margin-bottom: unset;
-      flex-flow: row wrap;
-      justify-content: flex-start;
-    }
-  }
-
   .benefits {
     color: ${(props) => props.theme['base-text']};
     display: flex;
     flex-flow: column nowrap;
-    gap: 20px 40px;
+    gap: 20px;
 
     @media ${breakpoints.md} {
+      gap: 40px;
       flex-flow: row wrap;
       align-items: center;
       justify-content: flex-start;
+    }
+
+    .left-container,
+    .right-container {
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
     }
   }
 `;
