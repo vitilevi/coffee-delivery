2a71ecf9002c5a14d74bb13b5feaa99f69f9f906
Victor Faria <vitilevi@hotmail.com>
2023-09-27 16:56:25 -0300

    Add styling to coffe section on home page

--- src/pages/Home/components/CoffeeSection/styles.ts
+++ src/pages/Home/components/CoffeeSection/styles.ts
@@ -1,3 +1,20 @@
 import styled from 'styled-components';
+import { breakpoints } from '../../../../utils/breakpoints';
 
-export const CoffeeSectionWrapper = styled.section``;
+export const CoffeeSectionWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding: 32px 0;
+`;
+
+export const CoffeesDisplay = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-flow: row wrap;
+  gap: 40px 32px;
+  margin-top: 54px;
+
+  @media ${breakpoints.dt} {
+    justify-content: flex-start;
+  }
+`;
